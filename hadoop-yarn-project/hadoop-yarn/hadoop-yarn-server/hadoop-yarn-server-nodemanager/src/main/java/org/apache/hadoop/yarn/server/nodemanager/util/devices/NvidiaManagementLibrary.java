3085308fc71ea0d7926b8b94d186091da3bcfc2a
robzor92 <robin.eric.andersson@gmail.com>
2017-02-12 19:48:16 +0100

    Add initialization and shutdown method in JNI api

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/util/devices/NvidiaManagementLibrary.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/util/devices/NvidiaManagementLibrary.java
@@ -7,16 +7,20 @@ public class NvidiaManagementLibrary {
         System.loadLibrary("devices");
     }
 
+    public static native boolean initialize();
+
+    public static native boolean shutDown();
+
     public static native String queryMandatoryDevices();
 
     public static native String queryAvailableDevices();
     
-    public static native boolean initialize();
-    
     public static void main(String [] args) {
+        NvidiaManagementLibrary.initialize();
           System.out.println("mandatory devs: " +
               NvidiaManagementLibrary.queryMandatoryDevices());
           System.out.println("available devs: " +
               NvidiaManagementLibrary.queryAvailableDevices());
+          NvidiaManagementLibrary.shutDown();
     }
 }
