5e3fa6332ab5c0a83b079eda5f60fe7b25240bfe
robzor92 <robin.eric.andersson@gmail.com>
2017-02-23 10:39:36 +0100

    Moved Device class

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/io/hops/devices/Device.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/io/hops/devices/Device.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.hops;
+package io.hops.devices;
 
 import java.util.Objects;
 
