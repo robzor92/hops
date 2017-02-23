9c61d5be187372c74389b4296ee7b677d4fdad64
robzor92 <robin.eric.andersson@gmail.com>
2017-02-23 10:40:19 +0100

    Moved in io.hops.devices package

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/io/hops/devices/GPUManagementLibraryLoader.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/io/hops/devices/GPUManagementLibraryLoader.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.yarn.server.nodemanager.util.devices;
+package io.hops.devices;
 
 import io.hops.GPUManagementLibrary;
 import io.hops.exceptions.GPUManagementLibraryException;
