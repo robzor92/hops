8abc2e83dbc84402e39de5351c5201e4a5520e59
robzor92 <robin.eric.andersson@gmail.com>
2017-04-28 11:29:42 +0200

    Add GPUAllocator allocates in a "fastest-gpu-first" policy instead of random

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/io/hops/devices/GPUAllocator.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/io/hops/devices/GPUAllocator.java
@@ -3,6 +3,7 @@ package io.hops.devices;
 
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Lists;
 import io.hops.GPUManagementLibrary;
 import io.hops.GPUManagementLibraryLoader;
 import io.hops.exceptions.GPUManagementLibraryException;
@@ -11,10 +12,13 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.yarn.api.records.ContainerId;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,7 +26,7 @@ public class GPUAllocator {
   
   final static Log LOG = LogFactory.getLog(GPUAllocator.class);
   private static final GPUAllocator gpuAllocator = new GPUAllocator();
-
+  
   private HashSet<Device> availableDevices;
   private HashMap<String, HashSet<Device>> containerDeviceAllocation;
   private HashSet<Device> mandatoryDevices;
@@ -35,7 +39,7 @@ public class GPUAllocator {
   public static GPUAllocator getInstance(){
     return gpuAllocator;
   }
-
+  
   private GPUAllocator() {
     availableDevices = new HashSet<>();
     containerDeviceAllocation = new HashMap<>();
@@ -44,8 +48,16 @@ public class GPUAllocator {
     try {
       gpuManagementLibrary =
           GPUManagementLibraryLoader.load(GPU_MANAGEMENT_LIBRARY_CLASSNAME);
-    } catch(GPUManagementLibraryException gpue) {
-      LOG.info("Could not load GPU management library");
+    } catch(GPUManagementLibraryException | UnsatisfiedLinkError e) {
+      LOG.error("Could not load GPU management library. Is this NodeManager " +
+          "supposed to offer its GPUs as a resource? If yes, check " +
+          "installation" +
+          " setup and make sure libnvidia-ml.so.1 is present in " +
+          "LD_LIBRARY_PATH. If no, disable GPUs by setting yarn.nodemanager" +
+          ".resource.gpus.enabled to " +
+          "false." + "\n");
+      
+      e.printStackTrace();
     }
   }
   
@@ -55,28 +67,28 @@ public class GPUAllocator {
     availableDevices = new HashSet<>();
     containerDeviceAllocation = new HashMap<>();
     mandatoryDevices = new HashSet<>();
-
+    
     this.gpuManagementLibrary = gpuManagementLibrary;
     initialize(configuredGPUs);
   }
-
+  
   /**
    * Initialize the NVML library to check that it was setup correctly
    * @return boolean for success or not
    */
   public boolean initialize(int configuredGPUs) {
-      if(initialized == false) {
-        initialized = gpuManagementLibrary.initialize();
-        initAvailableDevices(configuredGPUs);
-        initMandatoryDevices();
-      }
+    if(initialized == false) {
+      initialized = gpuManagementLibrary.initialize();
+      initAvailableDevices(configuredGPUs);
+      initMandatoryDevices();
+    }
     return this.initialized;
   }
   
   public boolean isInitialized() {
     return this.initialized;
   }
-
+  
   /**
    * Shut down NVML by releasing all GPU resources previously allocated with nvmlInit()
    * @return boolean for success or not
@@ -87,7 +99,7 @@ public class GPUAllocator {
     }
     return false;
   }
-
+  
   /**
    * Queries NVML to discover device numbers for mandatory driver devices that
    * all GPU containers should have access to
@@ -180,23 +192,16 @@ public class GPUAllocator {
       //deny devices already in use
       cGroupDeviceMapping.put("deny", currentlyAllocatedGPUs);
       
-      HashSet<Device> deviceAllocation = new HashSet<>();
-      Iterator<Device> itr = availableDevices.iterator();
-      while(gpus != 0) {
-        Device gpu = itr.next();
-        itr.remove();
-        deviceAllocation.add(gpu);
-        gpus--;
-      }
+      HashSet<Device> deviceAllocation = selectDevicesToAllocate(gpus);
       
       LOG.info("Gpus to allocate for " + containerName + " = " +
           deviceAllocation);
-        
+      
       containerDeviceAllocation.put(containerName, deviceAllocation);
       //only allow access to allocated GPUs
       cGroupDeviceMapping.put("allow", deviceAllocation);
-
-      //need to deny remaining available devices
+      
+      //deny remaining available devices
       cGroupDeviceMapping.get("deny").addAll(availableDevices);
       LOG.info("Gpus to deny for " + containerName + " = " +
           cGroupDeviceMapping.get("deny"));
@@ -206,6 +211,42 @@ public class GPUAllocator {
       throw new IOException("Container " + containerName + " requested " +
           gpus + " GPUs when only " + availableDevices.size() + " available");
     }
+  }
+  
+  /**
+   * This method selects GPUs to allocate based on a "Fastest GPU first" policy
+   * Device compute capability of NVIDIA GPUs are sorted based on the
+   * minor device number. - "The lower the number the faster the device.
+   *
+   * TODO: In the future this method could support different selection policies
+   * TODO: such as "Random GPU", "Slowest GPU first" or something
+   * TODO: actually useful like topology-based selection with GPUs
+   * TODO: interconnected using NVLink.
+   * @param gpus
+   * @return set of GPU devices that have been allocated
+   */
+  private synchronized HashSet<Device> selectDevicesToAllocate(int gpus) {
+    
+    HashSet<Device> deviceAllocation = new HashSet<>();
+    Iterator<Device> availableDeviceItr = availableDevices.iterator();
+    
+    TreeSet<Integer> minDeviceNums = new TreeSet<>();
+    
+    while(availableDeviceItr.hasNext()) {
+      minDeviceNums.add(availableDeviceItr.next().getMinorDeviceNumber());
+    }
+    
+    Iterator<Integer> minDeviceNumItr = minDeviceNums.iterator();
+    
+    while(minDeviceNumItr.hasNext() && gpus != 0) {
+      int deviceNum = minDeviceNumItr.next();
+      Device allocatedGPU = new Device(NVIDIA_GPU_MAJOR_DEVICE_NUMBER, deviceNum);
+      availableDevices.remove(allocatedGPU);
+      deviceAllocation.add(allocatedGPU);
+      gpus--;
+    }
+    
+    return deviceAllocation;
   }
   
   /**
@@ -220,7 +261,7 @@ public class GPUAllocator {
     availableDevices.addAll(deviceAllocation);
     containerDeviceAllocation.remove(containerName);
   }
-   
+  
   /**
    * Given the containerId and the Cgroup contents for devices.allow
    * extract allocated GPU devices
@@ -240,21 +281,21 @@ public class GPUAllocator {
    * Use a simple pattern that splits on the two spaces, and
    * grabs the 2nd field
    */
-  private static final Pattern DEVICES_ALLOW_FORMAT = Pattern.compile(
+  private static final Pattern DEVICES_LIST_FORMAT = Pattern.compile(
       "([^\\s]+)+\\s([\\d+:\\d]+)+\\s([^\\s]+)");
   
   /**
-   * Find GPU devices in the contents of the devices.allow file
+   * Find GPU devices in the contents of the devices.list file
    * This method is used in the recovery process and will only filter out the
-   * Nvidia GPUs (major device number 195)
+   * NVIDIA GPUs (major device number 195)
    * @param devicesAllowStr
    * @return
    */
   private HashSet<Device> findGPUDevices(String devicesAllowStr) {
     HashSet<Device> devices = new HashSet<>();
-  
-    Matcher m = DEVICES_ALLOW_FORMAT.matcher(devicesAllowStr);
-  
+    
+    Matcher m = DEVICES_LIST_FORMAT.matcher(devicesAllowStr);
+    
     while (m.find()) {
       String majorMinorDeviceNumber = m.group(2);
       String[] majorMinorPair = majorMinorDeviceNumber.split(":");
